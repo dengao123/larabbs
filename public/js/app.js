98c65f4e96ec44b98f58e0247b925004913ce7c9
dengao <2916012032@qq.com>
2019-08-01 19:09:55 +0800

    修复跳转链接

--- public/js/app.js
+++ public/js/app.js
@@ -49366,7 +49366,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/larabbs/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! larabbs/app/vagrant/Code/larabbs/resources/js/app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! /home/vagrant/Code/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
